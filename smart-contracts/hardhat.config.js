885e5c5a92a41bf1c2e7afdba6e0e1bb308ddc16
Devansh Tiwari <devanshtiwari365@gmail.com>
2022-04-10 01:17:18 +0530

    Add Smart contract for minting images

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -2,4 +2,12 @@ require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url:'https://eth-rinkeby.alchemyapi.io/v2/4Ln0V8uKbWDNDS1iVDt9LQClOWiMqN2y',
+      accounts: [
+        '997b5147805a17a2a2b1202fa9bfd537abe6453720b2739ed54eef760b88f150',
+      ]
+    }
+  }
 };
